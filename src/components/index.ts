52233b0c6f4417f69754be4a376e526ce55b5b52
dayana2100 <dayana.motta.camayo@gmail.com>
2025-10-20 07:58:45 -0500

    CORTE 3

---
+++ src/components/index.ts
@@ -0,0 +1,14 @@
+
+
+export * from './products/producto-grid/ProductGrid';
+export * from './products/producto-grid/ProductGridItem';
+
+export * from './product/size-selector/SizeSelector';
+export * from './product/quantity-selector/QuantitySelector';
+
+export * from './product/slideshow/SlideShowProduct';
+
+export * from './ui/top-menu/TopMenu';
+export * from './ui/not-found/NotFoundPage';
+export * from './ui/title/Title';
+export * from './ui/sidebar/Sidebar';
